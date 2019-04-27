ffd701fb619661181257f4eb29caf86e6c906313
Anwesh43 <anweshthecool0@gmail.com>
2019-04-27 11:03:58 +0530

    adding State class

--- bicirclestepfillview/src/main/java/com/anwesh/uiprojects/bicirclestepfillview/BiCircleFillStepView.kt
+++ bicirclestepfillview/src/main/java/com/anwesh/uiprojects/bicirclestepfillview/BiCircleFillStepView.kt
@@ -64,7 +64,7 @@ fun Canvas.drawBCFSNode(i : Int, scale : Float, paint : Paint) {
 class BiCircleFillStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    
+
     override fun onDraw(canvas : Canvas) {
 
     }
@@ -76,5 +76,25 @@ class BiCircleFillStepView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scale.updateValue(dir, circles, circles)
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }

6d3b0c1fdc5d033e2916f354cba614c08f20991a
Anwesh43 <anweshthecool0@gmail.com>
2019-04-27 10:43:02 +0530

    adding libraries and constants

--- bicirclestepfillview/src/main/java/com/anwesh/uiprojects/bicirclestepfillview/BiCircleFillStepView.kt
+++ bicirclestepfillview/src/main/java/com/anwesh/uiprojects/bicirclestepfillview/BiCircleFillStepView.kt
@@ -4,3 +4,21 @@ package com.anwesh.uiprojects.bicirclestepfillview
  * Created by anweshmishra on 27/04/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+
+val nodes : Int = 5
+val circles : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
